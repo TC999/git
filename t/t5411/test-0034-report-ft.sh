421e7a9b4c80ca6dec9f1356382d2c01766f621f
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2021-01-20 21:53:31 -0500

    t5411: refactor check of refs using test_cmp_refs
    Add new helper 'test_cmp_refs' to check references in a repository.
    
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5411/test-0034-report-ft.sh
+++ t/t5411/test-0034-report-ft.sh
@@ -28,13 +28,11 @@ test_expect_success "proc-receive: fall throught, let receive-pack to execute ($
 	 * [new reference] <COMMIT-B> -> refs/for/master/topic
 	EOF
 	test_cmp expect actual &&
-	git -C "$upstream" show-ref >out &&
-	make_user_friendly_and_stable_output <out >actual &&
-	cat >expect <<-EOF &&
+
+	test_cmp_refs -C "$upstream" <<-EOF
 	<COMMIT-B> refs/for/master/topic
 	<COMMIT-A> refs/heads/master
 	EOF
-	test_cmp expect actual
 '
 
 # Refs of upstream : master(A)             refs/for/master/topic(A)
