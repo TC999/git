14903dbd206ea597e3bc20e82e863119f7422bad
Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>
2022-07-02 11:30:18 +0800

    Add cmd parameters with cobra
    Prepare the cmd parameters and options, for call commands.
    
    Signed-off-by: Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>

--- cmd/root.go
+++ cmd/root.go
@@ -7,9 +7,90 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var AGitOptions Options
+
+type Options struct {
+	CurrentPath      string
+	ReleaseBranch    string
+	GitTargetVersion string
+	UseLocal         bool
+	UseRemote        bool
+
+	GitVersion  string
+	AGitVersion string
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "",
 	Short: "",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("execute")
+	},
+}
+
+func init() {
+	Options := &Options{}
+	currentPath, err := os.Getwd()
+	if err != nil {
+		panic("cannot get current path: " + err.Error())
+	}
+
+	// If not provide the path, then it will use current path
+	rootCmd.Flags().StringVarP(
+		&Options.CurrentPath,
+		"path",
+		"p",
+		currentPath,
+		"",
+	)
+
+	rootCmd.Flags().StringVarP(
+		&Options.ReleaseBranch,
+		"release-branch",
+		"r",
+		"",
+		"the release branch name",
+	)
+
+	rootCmd.Flags().StringVarP(
+		&Options.GitTargetVersion,
+		"target-version",
+		"t",
+		"",
+		"v2.36.1",
+	)
+
+	rootCmd.Flags().BoolVarP(
+		&Options.UseRemote,
+		"use-remote",
+		"u",
+		false,
+		"if local and remote not same, then use remote branch",
+	)
+
+	rootCmd.Flags().BoolVarP(
+		&Options.UseLocal,
+		"use-local",
+		"l",
+		false,
+		"if local and remote not same, then use local branch",
+	)
+
+	rootCmd.Flags().StringVarP(
+		&Options.GitVersion,
+		"git-version",
+		"",
+		"",
+		"the git version, such as v2.36.1",
+	)
+
+	rootCmd.Flags().StringVarP(
+		&Options.AGitVersion,
+		"agit-version",
+		"",
+		"",
+		"the agit version, such as 6.5.9",
+	)
 }
 
 func Execute() {
