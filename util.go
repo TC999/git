2416c82898e4c520496998c31f6cceb1cb717460
Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>
2022-07-02 13:27:08 +0800

    Add command execute helper methods
    The command execute helper methods, use NewCommand to create a new
    command object, which will be used to execute command.
    
    Signed-off-by: Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>

--- util.go
+++ util.go
@@ -1,8 +1,11 @@
 package agit_release
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 )
 
 // CheckFileExist will check the paths exist, if one not exist,
@@ -15,4 +18,22 @@ func CheckFileExist(paths ...string) error {
 	}
 
 	return nil
+}
+
+// NewCommand will return space-command for advantage use,
+// You must get stdin, stdout and stderr before spaceCommand.Wait(), and do not miss spaceCommand.Wait()
+func NewCommand(ctx context.Context, path string, env []string, stdin io.Reader, stdout, stderr io.Writer,
+	commandName string, args ...string) (Commander, error) {
+	cmd := exec.Command(commandName, args...)
+
+	if len(path) > 0 {
+		cmd.Dir = path
+	}
+
+	agitCommand, err := new(ctx, cmd, stdin, stdout, stderr, env...)
+	if err != nil {
+		return nil, err
+	}
+
+	return agitCommand, nil
 }
