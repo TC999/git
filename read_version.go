d2867463d5f374e90c64e23e0846c0330bbcfa77
Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>
2022-07-03 20:09:30 +0800

    Implement read version files task
    This implement to read 'GIT-VERSION' and 'PATCH-VERSION' file for get
    the git version and agit version.
    
    Signed-off-by: Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>

---
+++ read_version.go
@@ -0,0 +1,71 @@
+package agit_release
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"path"
+
+	"golang.aliyun-inc.com/agit/agit-release/cmd"
+)
+
+const (
+	_gitVersion  = "GIT-VERSION"
+	_agitVersion = "PATCH-VERSION"
+)
+
+type AGitVersion struct {
+	next     Scheduler
+	nextName string
+}
+
+func (a *AGitVersion) Do(o *cmd.Options) error {
+	if o.GitVersion != "" && o.AGitVersion != "" {
+		return nil
+	}
+
+	log.Print("will get git and agit version from current path files")
+
+	gitVersionPath := path.Join(o.CurrentPath, _gitVersion)
+	agitVersionPath := path.Join(o.CurrentPath, _agitVersion)
+
+	if err := CheckFileExist(gitVersionPath, agitVersionPath); err != nil {
+		return err
+	}
+
+	gitVersion, err := os.ReadFile(gitVersionPath)
+	if err != nil {
+		return fmt.Errorf("read git version failed, err: %v", err)
+	}
+
+	agitVersion, err := os.ReadFile(agitVersionPath)
+	if err != nil {
+		return fmt.Errorf("read agit verison failed, err: %v", err)
+	}
+
+	gitVersion = bytes.TrimSpace(gitVersion)
+	agitVersion = bytes.TrimSpace(agitVersion)
+
+	if len(gitVersion) <= 0 {
+		return fmt.Errorf("git version is empty")
+	}
+
+	if len(agitVersion) <= 0 {
+		return fmt.Errorf("agit version is empty")
+	}
+
+	o.GitVersion = string(gitVersion)
+	o.AGitVersion = string(agitVersion)
+	return nil
+}
+
+func (a AGitVersion) Next(scheduler Scheduler, name string) error {
+	if scheduler != nil {
+		return fmt.Errorf("the %s is nil", name)
+	}
+
+	a.next = scheduler
+	a.nextName = name
+	return nil
+}
