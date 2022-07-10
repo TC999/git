80a842302edb9263ff1f4c4f37abc5a22db77953
Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>
2022-07-10 18:33:12 +0800

    Implment TaskTopicTest for generate topic test files
    Will find all the topic test and write to series_test file.
    
    Signed-off-by: Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>

---
+++ task_topic_test_files.go
@@ -0,0 +1,122 @@
+package agit_release
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+)
+
+const (
+	_testFileReg = "(t/[^/]*.sh)"
+)
+
+const (
+	_testSeriesFile = "patches/series_test"
+)
+
+type TaskTopicTest struct {
+	next     Scheduler
+	nextName string
+}
+
+func (t *TaskTopicTest) Do(o *Options, taskContext *TaskContext) error {
+	if err := t.writeTestSeriesFile(o, taskContext); err != nil {
+		return err
+	}
+
+	if t.next != nil {
+		return t.next.Do(o, taskContext)
+	}
+
+	return nil
+}
+
+func (t *TaskTopicTest) Next(scheduler Scheduler, name string) error {
+	if scheduler == nil {
+		return fmt.Errorf("the scheduler named '%s' is nil", name)
+	}
+
+	t.nextName = name
+	t.next = scheduler
+	return nil
+}
+
+func (t *TaskTopicTest) writeTestSeriesFile(o *Options, taskContext *TaskContext) error {
+	testFiles, err := t.getTestListFromTopic(o, taskContext)
+	if err != nil {
+		return err
+	}
+
+	testSeriesFilePath := filepath.Join(o.CurrentPath, _testSeriesFile)
+
+	os.Remove(testSeriesFilePath)
+
+	contents := strings.Join(testFiles, "\n")
+
+	fmt.Print("Starting to write t/series_test file")
+
+	f, err := os.OpenFile(testSeriesFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	f.WriteString(contents)
+
+	fmt.Print("\tdone\n\n")
+
+	return nil
+}
+
+func (t *TaskTopicTest) getTestListFromTopic(o *Options, taskContext *TaskContext) ([]string, error) {
+	var res []string
+	if len(taskContext.topics) == 0 {
+		return nil, fmt.Errorf("not found topic")
+	}
+
+	if len(o.GitVersion) == 0 {
+		return nil, fmt.Errorf("not set --git-version")
+	}
+
+	for _, topic := range taskContext.topics {
+		tmpTestFiles, err := t.getTopicTestFiles(o, topic.GitBranch.BranchName)
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, tmpTestFiles...)
+	}
+
+	return res, nil
+}
+
+// getTopicTestFiles get test files from topic
+// it just gets the first level on test which path start with 't/',
+// such as 't/t0940-crypto-repository.sh'
+func (t *TaskTopicTest) getTopicTestFiles(o *Options, topicBranchName string) ([]string, error) {
+	var (
+		stdout bytes.Buffer
+		stderr bytes.Buffer
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	cmd, err := NewCommand(ctx, o.CurrentPath, nil, nil, &stdout, &stderr,
+		"git", "diff", "--name-only",
+		fmt.Sprintf("%s..%s", o.GitVersion, topicBranchName))
+	if err != nil {
+		return nil, fmt.Errorf("get test files from topic: '%s' failed, err: %v", topicBranchName, err)
+	}
+
+	if err = cmd.Wait(); err != nil {
+		return nil, fmt.Errorf("get test files from topic: '%s' failed, stderr: %s", topicBranchName, stderr.String())
+	}
+
+	reg := regexp.MustCompile(_testFileReg)
+	return reg.FindAllString(stdout.String(), -1), nil
+}
